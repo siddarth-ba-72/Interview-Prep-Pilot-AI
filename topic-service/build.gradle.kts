b702cb493d989fc09efeb2668b1561af9dcf57f0
siddarth-ba-72 <siddarth.ba02@gmail.com>
2026-08-01 18:54:06 +0530

    actuators for backend

--- topic-service/build.gradle.kts
+++ topic-service/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
